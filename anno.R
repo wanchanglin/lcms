186f4a684566165ac9d9d1a6d3835e1edf7a796a
harryln <harryln66@hotmail.com>
2018-03-23 12:27:37 +0000

    debug and test for deisotoping and annotating

--- anno.R
+++ anno.R
@@ -1,11 +1,9 @@
 ## wl-12-03-2018, Mon: commence 
 ## wl-15-03-2018, Thu: tidy R codes
 ## wl-21-03-2018, Wed: major changes
+## wl-23-03-2018, Fri: test and debug deisotoping and annotating
 ## To-Do
-##  1) modify deisotoping
-##  2) modify annotating
-##  3) modify makelibrary
-##  4) test and debug
+##  1) test and debug makelibrary
 
 library(xcms)
 
@@ -169,43 +167,40 @@ makelibrary <- function(ionisation_mode, lookup_lipid_class,lookup_FA,
   return(library)
 }
 
+## wl-23-03-2018, Fri: debug and tidy up
 deisotoping <- function(ppm=5, no_isotopes=2, prop.1=0.9, prop.2=0.5, 
                         spectra=spectra){
 
   C13_1 <- 1.003355
   C13_2 <- C13_1*2
-
-  k = 0
-  m = 0
+  k     <- 0
+  m     <- 0
 
   ## run loop to find isotopes for each ion.
   for(i in (1:(nrow(spectra)-1))){
     ## values of search
-    mass <- as.numeric(spectra[i,1])
+    mass      <- as.numeric(spectra[i,1])
     intensity <- as.numeric(spectra[i,2])
     ## calculated values
-    offset = (ppm * mass) / 1000000
+    offset <- (ppm * mass) / 1000000
 
     ## find isotope with ppm filter on isotpe
     search <- round((mass+C13_1),digits = 3)
-    top <- search + offset
+    top    <- search + offset
     bottom <- search - offset
     result <- spectra[as.numeric(spectra[,"intensity"]) <= (intensity*prop.1) & spectra[,1] >= bottom & spectra[,1] <= top & spectra[,"isotope"] == "",]
     result <- rbind(result,blank1 = "", blank2 = "")
 
-
-    if(no_isotopes ==2){
+    if(no_isotopes == 2){
       ## find isotope with ppm filter on isotpe
-      search <- round((mass+C13_2),digits = 3)
-      top <- search + offset
-      bottom <- search - offset
+      search   <- round((mass+C13_2),digits = 3)
+      top      <- search + offset
+      bottom   <- search - offset
       result_2 <- spectra[as.numeric(spectra[,"intensity"]) <= (intensity*prop.2) & spectra[,1] >= bottom & spectra[,1] <= top & spectra[,"isotope"] == "",]
       result_2 <- rbind(result_2,blank1 = "", blank2 = "")
-
     }
 
-
-    if(nrow(result)>2){
+    if(nrow(result)> 2){
       k = k +1
       spectra[i, "isotope"] <- paste(spectra[i, "isotope"]," ", "[",k,"]","[M]", sep="")
       for(j in 1:(nrow(result)-2)){
@@ -222,18 +217,28 @@ deisotoping <- function(ppm=5, no_isotopes=2, prop.1=0.9, prop.2=0.5,
 
   }
 
-  allpeaks <- as.data.frame(spectra)
-  deisotoped  <- allpeaks[(grep("\\[M\\+", allpeaks$isotope, invert = T)),]
-  isotopes  <- allpeaks[(grep("\\[M\\+", allpeaks$isotope, invert = F)),]
-  results<-list(allpeaks, deisotoped, isotopes)
-  summary <- paste(length(as.vector(deisotoped$mz.obs)),"monoisotopic peaks retained and", length(as.vector(isotopes$mz.obs)), "C13 isotopes discarded from", length(as.vector(allpeaks$mz.obs)), "detected ions", sep=" ")
-  print(summary); log<-c(log,summary)
+  allpeaks   <- as.data.frame(spectra)
+  deisotoped <- allpeaks[(grep("\\[M\\+", allpeaks$isotope, invert = T)),]
+  isotopes   <- allpeaks[(grep("\\[M\\+", allpeaks$isotope, invert = F)),]
+  results    <- list(allpeaks, deisotoped, isotopes)
+  summary    <- paste(length(as.vector(deisotoped$mz.obs)),
+                      "monoisotopic peaks retained and", 
+                      length(as.vector(isotopes$mz.obs)), 
+                      "C13 isotopes discarded from", 
+                      length(as.vector(allpeaks$mz.obs)), 
+                      "detected ions", sep=" ")
+  print(summary)
+
   return(deisotoped)
 }
 
+## wl-23-03-2018, Fri: Debug and tidy up. 
+## Notes: No ionisation mode control inside the function. Use ionisation
+##        mode to control 'adducts' outside this function.
 annotating <- function(deisotoped,
                        adducts=c(H=T, NH4=F, Na=T, K=F, dH=F, Cl=F, OAc=F),
                        ppm.annotate=10, dbase){
+
   print("Starting annotation")
   d.finalmz   <- as.vector(deisotoped$mz.obs)
   s1          <- dbase
@@ -243,20 +248,22 @@ annotating <- function(deisotoped,
   index       <- 13 # offset to search only rounded masses in library
 
   for(a in 1:length(adducts)){
-    if(adducts[a]==T) sel.adducts<- c(sel.adducts, index+a)
+    if(adducts[a]==T) sel.adducts <- c(sel.adducts, index+a)
   }
 
   for(i in 1:nrow(spectra)){
-    search<-as.numeric(spectra[i,1])
-    offset = (ppm.annotate * search) / 1000000
-    top <- search + offset
+    search <- as.numeric(spectra[i,1])
+    offset <- (ppm.annotate * search) / 1000000
+    top    <- search + offset
     bottom <- search - offset
-    result<-which( s1[sel.adducts,] >= bottom & s1[sel.adducts,] <= top ,arr.ind=TRUE)
+    result <- which( s1[sel.adducts,] >= bottom & s1[sel.adducts,] <= top ,arr.ind=TRUE)
+
     if(nrow(result)>0){
-      for(j in 1:nrow(result))
-      {
-        col<-result[j,"col"]
-        row<-result[j,"row"]; row<-sel.adducts[row]
+      for(j in 1:nrow(result)) {
+        col <- result[j,"col"]
+        row <- result[j,"row"]
+        row <- sel.adducts[row]
+
         ## determine the adduct that was matched, summarising match information from library for matched mass (as 'data')
         ##determine which adduct
         if(row == "14"){adduct <- "protonated"; name.adduct <- "H"}
@@ -267,35 +274,47 @@ annotating <- function(deisotoped,
         if(row == "19"){adduct <- "chlorinated"; name.adduct <- "Cl"}
         if(row == "20"){adduct <- "acetate"; name.adduct <- "OAc"}
 
-        a.ppm = round(abs(((as.numeric(spectra[i,2]) - as.numeric(s1[adduct,col])) / as.numeric(spectra[i,2]))* 1000000), digits=1)
+        a.ppm <- round(abs(((as.numeric(spectra[i,2]) - as.numeric(s1[adduct,col])) / as.numeric(spectra[i,2]))* 1000000), digits=1)
 
         ##make vector with summary of match and paired match
-        data<- c(s1[row,col],  s1[adduct,col],spectra[i,2], a.ppm, s1["formula",col], name.adduct,  s1["protonated",col],  s1["FA1",col], s1["FA2",col], s1["FA3",col])
+        data <- c(s1[row,col],  s1[adduct,col],spectra[i,2], a.ppm, 
+                  s1["formula",col], name.adduct,  s1["protonated",col],  
+                  s1["FA1",col], s1["FA2",col], s1["FA3",col])
 
         ##make matrix of search results
         combined <- rbind(combined,unlist(data, use.names=F))
       }
     }
   }
-  if(length(combined)>0){
-    colnames(combined) <- c("mz.matched",  "mz.matched.lib", "mz.observed", "ppm", "formula", "adduct", "mz.lib.protonated", "FA1", "FA2", "FA3")
 
-    ids <- unique.matrix(combined[,c(3, 5,6)])
-    annotations<-cbind(d.finalmz, "")
+  if(length(combined) > 0){
+    colnames(combined) <- c("mz.matched",  "mz.matched.lib", "mz.observed", 
+                            "ppm", "formula", "adduct", "mz.lib.protonated", 
+                            "FA1", "FA2", "FA3")
+
+    ids         <- unique.matrix(combined[,c(3, 5,6)])
+    annotations <- cbind(d.finalmz, "")
+
     for(i in 1:nrow(annotations)){
       result <- which(ids[,1] == annotations[i,1], arr.ind = T)
       if(length(result)>0){
         for(j in 1:length(result)){
-          annotations[i,2]<-paste(annotations[i,2],"[",ids[result[j],"formula"],"+",ids[result[j],"adduct"],"]", sep="")
+          annotations[i,2] <- paste(annotations[i,2],"[",
+                                    ids[result[j],"formula"],"+",
+                                    ids[result[j],"adduct"],"]", sep="")
         }
       }
     }
 
-    summary <- paste(length(annotations[annotations[,2] !="",2]),"from",length(as.vector(deisotoped$mz.obs)),"monoisotopic peaks were annoated (using accuract mass) with a",ppm.annotate,"ppm tollerance", sep=" "); log<-c(log,summary)
+    summary <- paste(length(annotations[annotations[,2] !="",2]),"from",
+                     length(as.vector(deisotoped$mz.obs)),
+                     "monoisotopic peaks were annoated (using accuract mass) with a",
+                     ppm.annotate,"ppm tollerance", sep=" ")
     print(summary)
 
     return(annotations[,2])
   }
+
 }
 
 ## ======================================================================== 
@@ -303,10 +322,21 @@ annotating <- function(deisotoped,
 
 home_dir        <- "C:/R_lwc/lcms/"         ## for windows
 lib_dir         <- paste0(home_dir,"libraries/")
-adducts         <- c(H = T, NH4 = T, Na = T, K = F, dH = F, Cl = F, OAc = F) 
-## choose which adduct you want to include in library search
 ppm.annotate    <- 15   # choose ppm for annotations
 ionisation_mode <- "positive"  # either "positive" or "negative" 
+
+## -------------------------------------------------------------------- 
+if (T) {
+  ## wl-23-03-2018, Fri: from 'annotate' of 'massPix'
+  if (ionisation_mode == "positive"){
+    adducts <- c(H = T, NH4 = F, Na = T, K = T, dH = F, Cl = F, OAc = F)
+  } else {
+    adducts <- c(H = F, NH4 = F, Na = F, K = F, dH = T, Cl = T, OAc = F)
+  }
+} else {
+  ## wl-23-03-2018, Fri: this the default value for 'annotating'
+  adducts  <- c(H = T, NH4 = T, Na = T, K = F, dH = F, Cl = F, OAc = F) 
+}
 
 ## ======================================================================== 
 ## Make library for annotation
@@ -353,6 +383,7 @@ colnames(spectra) <- c("mz.obs", "intensity", "isotope", "modification" )
 ## -------------------------------------------------------------------  
 deisotoped <- deisotoping(ppm=5, no_isotopes=2, prop.1=0.9, prop.2=0.5,
                           spectra=spectra)
+
 annotated  <- annotating(deisotoped, adducts, ppm.annotate, dbase)
 
 ## =======================================================================
