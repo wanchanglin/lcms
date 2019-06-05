d38a4de919bd6c34b306e535353cdaf90fc5cb6a
wanchanglin <wanchang_lin@outlook.com>
2019-06-05 13:18:28 +0100

    test again

---
+++ test/lcms_pos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# wl-05-06-2019, Wed: use file names for mzxml_file
+
+Rscript --vanilla ../lcms.R \
+  --process T \
+  --mzxml_file "../test-data/lcms_pos/ZH_190918_mann_liver_pos_001.mzML, \
+                ../test-data/lcms_pos/ZH_190918_mann_liver_pos_002.mzML, \
+                ../test-data/lcms_pos/ZH_190918_mann_liver_pos_003.mzML, \
+                ../test-data/lcms_pos/ZH_190918_mann_liver_pos_004.mzML, \
+                ../test-data/lcms_pos/ZH_190918_mann_liver_pos_005.mzML, \
+                ../test-data/lcms_pos/ZH_190918_mann_liver_pos_006.mzML, \
+                ../test-data/lcms_pos/ZH_190918_mann_liver_pos_007.mzML" \
+  --ionisation_mode "positive" \
+  --peak_out "../test-data/res/peak_pos.tsv" \
+  --rdata TRUE\
+  --rdata_out "../test-data/res/xset_pos.rdata"
